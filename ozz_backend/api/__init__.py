69741f7107eb27870f5e72faed8eb1ce2ab3cdb0
honeybee <honeybee@lab4dx.co.kr>
2022-12-12 20:09:44 +0900

    add getting material data api

--- ozz_backend/api/__init__.py
+++ ozz_backend/api/__init__.py
@@ -1,1 +1,2 @@
 from ozz_backend.api import mission
+from ozz_backend.api import material
--- ozz_backend/api/mission.py
+++ ozz_backend/api/mission.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from starlette.requests import Request
 
 from ozz_backend import app_logger
-from ozz_backend.persistence_layer.mission import Mission
+from ozz_backend.persistence_layer import Mission
 
 
 router = APIRouter(
@@ -18,6 +18,7 @@ class MissionInfoIn(BaseModel):
 
 
 class MissionInfoOut(BaseModel):
+    id: int
     title: str
     description: str
     material_id: int | None = None
@@ -35,6 +36,6 @@ def test_api():
 def get_mission(request: Request, mission_type: str):
     app_logger.info(f'[{request.method}] {request.url}: {request.client.host}:{request.client.port}')
     result = Mission.get_mission_info(mission_type)
-    return MissionInfoOut(title=result.title, description=result.description,
+    return MissionInfoOut(id=result.id, title=result.title, description=result.description,
                           material_id=result.material_id, knowhow_id=result.knowhow_id,
                           quest_id=result.quest_id).dict()
