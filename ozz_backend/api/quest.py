7ab96d450d31b21dfa7f206e7f2811440b786581
honeybee <honeybee@lab4dx.co.kr>
2022-12-19 22:13:26 +0900

    add user quest log api (temp)

--- ozz_backend/api/quest.py
+++ ozz_backend/api/quest.py
@@ -13,6 +13,10 @@ router = APIRouter(
 )
 
 
+class MessageOut(BaseModel):
+    message: str
+
+
 class QuestIn(BaseModel):
     mission_id: str
 
@@ -32,6 +36,12 @@ class QuestOutList(BaseModel):
     quests: list[dict]
 
 
+class UserQuest(BaseModel):
+    user_id: int
+    mission_id: int
+    status: str
+
+
 @router.get('/test')
 def test_api():
     app_logger.info('test')
@@ -39,9 +49,9 @@ def test_api():
 
 
 @router.get('/mission-quests', response_model=QuestOutList)
-def get_materials(request: Request, mission_id: int):
+def get_quests(request: Request, mission_id: int):
     app_logger.info(f'[{request.method}] {request.url}: {request.client.host}:{request.client.port}')
-    results = Quest.get_quest_with_mission_id(mission_id)
+    results = Quest.get_quests_with_mission_id(mission_id)
     quests = [QuestOut(id=result.id,
                        quest_name=result.quest_name,
                        description=result.description,
@@ -51,3 +61,27 @@ def get_materials(request: Request, mission_id: int):
                        link_quest_id=result.link_quest_id,
                        link_quest_cd=result.link_quest_cd).dict() for result in results]
     return {'quests': quests}
+
+
+# TODO : insert into user_quest_log_tb
+# when user start mission, add related data in user_mission_log_tb
+@router.post('/user-log', response_model=MessageOut)
+def add_user_mission_log(request: Request, ongoing_data: UserQuest):
+    app_logger.info(f'[{request.method}] {request.url}: {request.client.host}:{request.client.port}')
+    # Quest.add_user_mission_log(user_id=ongoing_data.user_id,
+    #                            mission_id=ongoing_data.mission_id,
+    #                            status=ongoing_data.status)
+    # return {'message': 'success to add'}
+    pass
+
+
+# TODO : update user_quest_log_tb
+# when user finish the mission, update status
+@router.patch('/user-log', response_model=MessageOut)
+def update_user_mission_log(request: Request, ongoing_data: UserQuest):
+    app_logger.info(f'[{request.method}] {request.url}: {request.client.host}:{request.client.port}')
+    # Quest.update_user_mission_log(user_id=ongoing_data.user_id,
+    #                               mission_id=ongoing_data.mission_id,
+    #                               status=ongoing_data.status)
+    # return {'message': 'success to update'}
+    pass
--- ozz_backend/persistence_layer/quest.py
+++ ozz_backend/persistence_layer/quest.py
@@ -6,7 +6,7 @@ from ozz_backend.database.conn import DBSession
 
 class Quest(object):
     @staticmethod
-    def get_quest_with_mission_id(mission_id):
+    def get_quests_with_mission_id(mission_id):
         # query-1
         # select
         #     qt.id, qt.quest_name, qt.description, qt.thumbnail_path,
@@ -34,15 +34,19 @@ class Quest(object):
                                entity.QuestTB.quest_name,
                                entity.QuestTB.description,
                                entity.QuestTB.thumbnail_path,
-                               entity.QuestBridgeTb.quest_cd,
-                               entity.QuestBridgeTb.quest_order,
-                               entity.QuestBridgeTb.link_quest_id) \
-            .join(entity.QuestBridgeTb, entity.QuestTB.id == entity.QuestBridgeTb.quest_id)\
-            .filter(entity.QuestBridgeTb.mission_id == mission_id).subquery()
+                               entity.QuestBridgeTB.quest_cd,
+                               entity.QuestBridgeTB.quest_order,
+                               entity.QuestBridgeTB.link_quest_id) \
+            .join(entity.QuestBridgeTB, entity.QuestTB.id == entity.QuestBridgeTB.quest_id)\
+            .filter(entity.QuestBridgeTB.mission_id == mission_id).subquery()
         sub_query = aliased(stmt, name='sub_query')
 
         return DBSession.query(sub_query.c.id, sub_query.c.quest_name, sub_query.c.description,
                                sub_query.c.thumbnail_path, sub_query.c.quest_cd, sub_query.c.quest_order,
-                               sub_query.c.link_quest_id, entity.QuestBridgeTb.quest_cd.label('link_quest_cd'))\
-            .outerjoin(entity.QuestBridgeTb, sub_query.c.link_quest_id == entity.QuestBridgeTb.id)\
+                               sub_query.c.link_quest_id, entity.QuestBridgeTB.quest_cd.label('link_quest_cd'))\
+            .outerjoin(entity.QuestBridgeTB, sub_query.c.link_quest_id == entity.QuestBridgeTB.id)\
             .order_by(sub_query.c.quest_order).all()
+
+    @staticmethod
+    def get_mission_quest():
+        pass
