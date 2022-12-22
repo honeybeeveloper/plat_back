50d82ece858c76ebf6aa814992e4683475ebafc7
honeybee <honeybee@lab4dx.co.kr>
2022-12-22 11:51:36 +0900

    cors allow_origins

--- ozz_backend/app.py
+++ ozz_backend/app.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # TODO : CORS 등록
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
